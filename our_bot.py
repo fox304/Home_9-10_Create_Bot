e8958988049397a05200e2e577703ceb37b346f0
Maks <nordmax69@gmail.com>
2022-12-08 10:45:50 +0700

    correct logging

--- our_bot.py
+++ our_bot.py
@@ -5,8 +5,8 @@ import logging
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO,
-    filename='log_book'
+    level=logging.DEBUG,
+    filename='log_book.log'
 )
 
 load_dotenv()
